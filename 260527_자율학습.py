9233ea6297511e99e5e37d4d50133c999efd67c8
drs650 <huborobot@gmail.com>
2026-05-27 17:12:19 +0900

    Add files via upload

--- 260527_자율학습.py
+++ 260527_자율학습.py
@@ -1,18 +1,18 @@
 import json
 
-with open("cafe_menu.json", "r", encoding="utf-8") as f:
+with open("260527/cafe_menu.json", "r", encoding="utf-8") as f:
     cafe_menu = json.load(f)
 
 while True:
     print("1.가격별 메뉴 조회 2.종류별 메뉴 조회 3.주문 0.종료 : ", end="")
-    num = int(input)
+    num = input()
     
     if num == '0':
         print("스마트 카페 메뉴조회 시스템 종료!")
         break
         
     elif num == '1':
-        price_input = int(input("최저가격, 최고가격 입력(예:1000,2000) "))
+        price_input = (input("최저가격, 최고가격 입력(예:1000,2000) "))
         
         min_price, max_price = map(int, price_input.split(','))
         
